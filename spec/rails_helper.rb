748f3a3f469218979b48843ededf5338ac5e5d52
dkulpa <d.kulpa@selleo.com>
2016-11-21 13:20:46 +0100

    added and configured Capybara, ffaker and FactoryGirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'shoulda/matchers'
+require 'capybara/rspec'
+require 'ffaker'
 
 ActiveRecord::Migration.maintain_test_schema!
 
